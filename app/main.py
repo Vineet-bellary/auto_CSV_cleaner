3e431ff1c05934a9fac932458831b526189eefff
Vineet-bellary <vineet.bellary@gmail.com>
2026-02-05 15:37:08 +0530

    Upload UI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from ui.upload import upload_csv
 
-st.set_page_config(page_title="Auto CSV Cleaner", page_icon=":broom:")
+st.set_page_config(page_title="Auto CSV Cleaner", page_icon=":broom:", layout="wide")
 
 st.title("Automatic CSV Data Cleaner")
-st.write("Upload your CSV to get Started")
+
+df = upload_csv()
+
+if df is not None:
+    st.write("### Dataset Info")
+    st.write(f"Rows: {df.shape[0]}")
+    st.write(f"Columns: {df.shape[1]}")
--- app/ui/upload.py
+++ app/ui/upload.py
@@ -5,3 +5,27 @@ Responsibilities:
 - Parsing using Pandas
 - Store in st.session_state
 """
+
+import streamlit as st
+import pandas as pd
+
+
+def upload_csv():
+    uploaded_file = st.file_uploader(
+        label="Upload your Dataset to begin (*.csv)", type=["csv"], key="data"
+    )
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+
+        # Storing in session state
+        st.session_state["df"] = df
+
+        st.success("CSV file uploaded successfully")
+
+        st.subheader("Data Preview")
+        st.dataframe(df.head())
+
+        return df
+
+    return None
